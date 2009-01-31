64a6d7efff32a0dd97560cbfb263ad62c55a91e5
Christian Theune <ct@gocept.com>
2009-01-31 12:44:17 +0000

    declare dependency on zope.datetime

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
     install_requires = ['setuptools',
                         'zope.annotation',
                         'zope.component',
+                        'zope.datetime',
                         'zope.interface',
                         ],
     zip_safe = False
