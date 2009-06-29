7e6071f1c78a67bf5d131309f2733541451f4604
Aaron Lehmann <aaron@zope.com>
2009-06-29 13:36:38 +0000

    Add 'zope.app.component' to the 'test' extra in the setup.py so that the creatorannotator tests can pass.

--- setup.py
+++ setup.py
@@ -36,7 +36,11 @@ setup(
     namespace_packages=['zope'],
     include_package_data=True,
     extras_require=dict(
-        test=['zope.testing', 'zope.security', 'zope.app.testing']
+        test=[
+            'zope.app.component',
+            'zope.testing',
+            'zope.security',
+            'zope.app.testing']
         ),
     install_requires = ['setuptools',
                         'zope.annotation',
